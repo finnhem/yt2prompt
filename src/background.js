d79407c07eed16407e7349967b04734015c728af
Finn Hemsteg <finnhemsteg@gmail.com>
2025-01-21 19:54:12 +0100

    Removed .js files.

--- src/background.js
+++
@@ -1,8 +0,0 @@
-// Listen for messages from content scripts
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === 'VIDEO_CHANGED') {
-    // Handle video change event if needed
-    console.log('Video changed:', message.videoId);
-  }
-  return true;
-}); 
